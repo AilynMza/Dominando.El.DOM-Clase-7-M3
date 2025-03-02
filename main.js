7b76a62d38fcd8df82fa1a81b4df7bbb4c2da033
AilynMza <ailynmorales501@gmail.com>
2025-03-02 03:24:41 -0600

    feat: cambio de background con imagen. Se crean funciones para obtener el número aleatorio entre un rango de valores y obtener un dato aleatorio de una cadena.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const rangeContainer = document.querySelector('.range-container');
 const inputRange = document.querySelector('.range-container input')
 const generateBtn = document.querySelector('.generator form button');
-
-
+    //      from  -  to
+    // Mayus 0 a     25
+    // Minus 26 a    51
+    // Numero 52 a   61
+    // Simbolos 62 a 67
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&*';
 
 const handleChange = () => {
     const inputValue = inputRange.value;
@@ -11,13 +15,43 @@ const handleChange = () => {
 }
 
 const generatePassword = (passwordLength) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&*';
+    let result = '';    
     for(let i = 0; i < passwordLength; i++){
         result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return result
 }
+
+// Función para obtener un número random entre dos rangos de valores
+const getRandomBetween = (from, to) => {
+    return from+(Math.floor(Math.random()*(to-from+1)));
+}
+
+// Obtener Mayus Aleatoria
+const getRandomMayus = () => {
+    const randomMayusIndex = getRandomBetween(0,25);
+    let characterMayus = characters.slice(randomMayusIndex, randomMayusIndex+1);
+    return characterMayus
+}
+// Obtener Minuscula Aleatoria
+const getRandomMinus = () => {
+    const randomMayusIndex = getRandomBetween(26,51);
+    let characterMayus = characters.slice(randomMinusIndex, randomMayusIndex+1);
+    return characterMinus
+}
+// Obtener Números Aleatorios
+const getRandomNumber = () => {
+    const randomMayusIndex = getRandomBetween(52,61);
+    let characterMayus = characters.slice(randomNumberIndex, randomMayusIndex+1);
+    return characterNumber
+}
+// Obtener Simbolos Aleatorios
+const getRandomSimbol = () => {
+    const randomMayusIndex = getRandomBetween(62,67);
+    let characterMayus = characters.slice(randomSimbolIndex, randomMayusIndex+1);
+    return characterSimbol
+}
+
 
 const printPassword = (event) => {
     event.preventDefault();
